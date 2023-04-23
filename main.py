94da297a50ffba40bbb71d656f0e0ff4e0744237
Dale.vand <advice02@naver.com>
2023-04-23 15:41:33 +0900

    🚀 데타 스페이스 배포를 위한 준비
    DB 테이블 생성 로직 추가

--- main.py
+++ main.py
@@ -8,6 +8,18 @@ import sqlite3
 con = sqlite3.connect('db.db',check_same_thread=False)
 cur = con.cursor()
 
+cur.execute(f"""
+            CREATE TABLE IF NOT EXISTS items (
+	            id INTEGER PRIMARY KEY,
+	            title TEXT NOT NULL,
+	            image BLOB,
+	            price INTEGER NOT NULL,
+	            description TEXT,
+	            place TEXT NOT NULL,
+	            insertAt INTEGER NOT NULL
+            );
+            """)
+
 app = FastAPI()
 
 @app.post('/items')
