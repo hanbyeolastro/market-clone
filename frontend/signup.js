ae68fe7e5a8a91fe5eada10492db5fcee5e81e46
Dale.vand <advice02@naver.com>
2023-04-23 18:14:45 +0900

    ✨ 회원 가입 로직 기능 구현
    프론트엔드 및 백엔드 작업 (signup)

---
+++ frontend/signup.js
@@ -0,0 +1,38 @@
+const form = document.querySelector("#signup-form");
+
+const checkPassword = () => {
+  const formData = new FormData(form);
+  const password1 = formData.get("password");
+  const password2 = formData.get("password2");
+
+  if (password1 === password2) {
+    return true;
+  } else return false;
+};
+
+const handleSubmit = async (event) => {
+  event.preventDefault();
+  const formData = new FormData(form);
+  const sha256Password = sha256(formData.get("password"));
+  formData.set("password", sha256Password);
+
+  const div = document.querySelector("#info");
+
+  if (checkPassword()) {
+    const res = await fetch("/signup", {
+      method: "post",
+      body: formData,
+    });
+    const data = await res.json();
+
+    if (data === "200") {
+      alert("회원 가입에 성공했습니다.");
+      window.location.pathname = "/login.html";
+    }
+  } else {
+    div.innerText = "비밀번호가 같지 않습니다.";
+    div.style.color = "red";
+  }
+};
+
+form.addEventListener("submit", handleSubmit);
--- main.py
+++ main.py
@@ -48,6 +48,17 @@ async def get_image(item_id):
     
     return Response(content=bytes.fromhex(image_bytes), media_type='image/*')
 
+@app.post('/signup')
+def signup(id:Annotated[str,Form()],
+           password:Annotated[str,Form()],
+           name:Annotated[str,Form()],
+           email:Annotated[str,Form()]):
+    cur.execute(f"""
+                INSERT INTO users(id,name,email,password)
+                VALUES ('{id}','{name}','{email}','{password}')
+                """)
+    con.commit()
+    return '200'
 
 app.mount("/", StaticFiles(directory="frontend", html=True), name="frontend")
 
