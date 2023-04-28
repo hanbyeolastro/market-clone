ecf661785458fb09ed33a9a38a49cf284652879c
Dale.vand <advice02@naver.com>
2023-04-28 23:04:54 +0900

    서버쪽 401 버그 수정

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI,UploadFile,Form,Response,Depends
+from fastapi import FastAPI, UploadFile, Form, Response, Depends
 from fastapi.security import OAuth2PasswordRequestForm
 from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
@@ -8,47 +8,53 @@ from fastapi_login.exceptions import InvalidCredentialsException
 from typing import Annotated
 import sqlite3
 
-con = sqlite3.connect('db.db',check_same_thread=False)
+con = sqlite3.connect('db.db', check_same_thread=False)
 cur = con.cursor()
 
 app = FastAPI()
 
 SERCRET = "super-coding"
-manager = LoginManager(SERCRET,'/login')
+manager = LoginManager(SERCRET, '/login')
+
 
 @manager.user_loader()
-def query_user(id):
+def query_user(data):
+    WHERE_STATEMENT = f'id="{data}"'
+    if type(data) == dict:
+        WHERE_STATEMENT = f'id="{data["id"]}"'
     con.row_factory = sqlite3.Row
     cur = con.cursor()
     user = cur.execute(f"""
-                       SELECT * from users WHERE id='{id}'
+                       SELECT * from users WHERE {WHERE_STATEMENT}
                        """).fetchone()
     return user
 
+
 @app.post('/login')
-def login(id:Annotated[str,Form()],
-           password:Annotated[str,Form()]):
+def login(id: Annotated[str, Form()],
+          password: Annotated[str, Form()]):
     user = query_user(id)
     if not user:
         raise InvalidCredentialsException
     elif password != user['password']:
         raise InvalidCredentialsException
-    
+
     access_token = manager.create_access_token(data={
-         'id':user['id'],
-        'name':user['name'],
-        'email':user['email']
+        'sub': {
+            'id': user['id'],
+            'name': user['name'],
+            'email': user['email']
+        }
     })
-     
-    return {'access_token':access_token}
-    
-               
+
+    return {'access_token': access_token}
+
 
 @app.post('/signup')
-def signup(id:Annotated[str,Form()],
-           password:Annotated[str,Form()],
-           name:Annotated[str,Form()],
-           email:Annotated[str,Form()]):
+def signup(id: Annotated[str, Form()],
+           password: Annotated[str, Form()],
+           name: Annotated[str, Form()],
+           email: Annotated[str, Form()]):
     cur.execute(f"""
                 INSERT INTO users(id,name,email,password)
                 VALUES ('{id}','{name}','{email}','{password}')
@@ -57,16 +63,15 @@ def signup(id:Annotated[str,Form()],
     return '200'
 
 
-
 @app.post('/items')
-async def create_item(image:UploadFile,
-                title:Annotated[str,Form()],
-                price:Annotated[int,Form()],
-                description:Annotated[str,Form()], 
-                place:Annotated[str,Form()],
-                insertAt:Annotated[int,Form()]
-                ):
-    
+async def create_item(image: UploadFile,
+                      title: Annotated[str, Form()],
+                      price: Annotated[int, Form()],
+                      description: Annotated[str, Form()],
+                      place: Annotated[str, Form()],
+                      insertAt: Annotated[int, Form()]
+                      ):
+
     image_bytes = await image.read()
     cur.execute(f"""
                 INSERT INTO 
@@ -75,8 +80,7 @@ async def create_item(image:UploadFile,
                 """)
     con.commit()
     return '200'
-    
-    
+
 
 @app.get('/items')
 async def get_items(user=Depends(manager)):
@@ -90,14 +94,14 @@ async def get_items(user=Depends(manager)):
 
     return JSONResponse(jsonable_encoder(dict(row) for row in rows))
 
+
 @app.get('/images/{item_id}')
 async def get_image(item_id):
     cur = con.cursor()
     image_bytes = cur.execute(f"""
                               SELECT image from items WHERE id={item_id}
                               """).fetchone()[0]
-    
+
     return Response(content=bytes.fromhex(image_bytes), media_type='image/*')
 
 app.mount("/", StaticFiles(directory="frontend", html=True), name="frontend")
-
